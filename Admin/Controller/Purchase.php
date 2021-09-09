b04a855586c02a7ce8d9c9ef4eb030dc892091cf
Luu Truong <truonglv@outlook.com>
2021-09-09 07:40:20 +0700

    Purchase reports

--- Admin/Controller/Purchase.php
+++ Admin/Controller/Purchase.php
@@ -128,6 +128,48 @@ class Purchase extends AbstractController
         );
     }
 
+    public function actionReports()
+    {
+        $visitor = \XF::visitor();
+        /** @var \DateTime|null $fromDate */
+        $fromDate = $this->filter('from', 'datetime,obj,tz:' . $visitor->timezone);
+        /** @var \DateTime|null $toDate */
+        $toDate = $this->filter('to', 'datetime,obj,tz:' . $visitor->timezone);
+
+        if ($toDate === null) {
+            $toDate = new \DateTime('@' . time(), new \DateTimeZone($visitor->timezone));
+            $toDate->modify('last day of this month');
+        }
+        if ($fromDate === null) {
+            $fromDate = new \DateTime('@' . $toDate->getTimestamp(), new \DateTimeZone($visitor->timezone));
+            $fromDate->modify('first day of this month');
+        }
+
+        $fromDate->setTime(0, 0, 0);
+        $toDate->setTime(23, 59, 59);
+
+        /** @var \Truonglv\XFRMCustomized\Repository\Report $reportRepo */
+        $reportRepo = $this->repository('Truonglv\XFRMCustomized:Report');
+        $data = $reportRepo->getReportsData($fromDate->getTimestamp(), $toDate->getTimestamp());
+
+        $dataJs = [];
+        $totalAmount = 0;
+        foreach ($data as $item) {
+            $dataJs[] = [$item['date'], (int) $item['amount']];
+            $totalAmount += $item['amount'];
+        }
+
+        $params = [
+            'reports' => $data,
+            'fromDate' => $fromDate->format('Y-m-d'),
+            'toDate' => $toDate->format('Y-m-d'),
+            'dataJs' => $dataJs,
+            'totalAmount' => $totalAmount
+        ];
+
+        return $this->view('Truonglv\XFRMCustomized:Purchase\Report', 'xfrmc_purchase_report', $params);
+    }
+
     protected function assertPurchaseExists(int $purchaseId): \Truonglv\XFRMCustomized\Entity\Purchase
     {
         /** @var \Truonglv\XFRMCustomized\Entity\Purchase $purchase */
